bb871fb78d57b5dfb002733a3594aa7870803990
fitnessflowgym-cyber <fitnessflowgym@gmail.com>
2025-12-23 13:12:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
+import axios from "axios";
 import userRoutes from "./routes/userRoutes.js";
 import exerciseRoutes from "./routes/exerciseRoutes.js";
 import workoutRoutes from "./routes/workoutRoutes.js";
@@ -20,7 +21,21 @@ app.use("/api/workouts", workoutRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/programs", programRoutes);
 
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
+  const url = `https://fitness-flow-backend-v4.onrender.com/ping`; // เปลี่ยนเป็น URL จริงของคุณ
+  setInterval(async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(`Self-ping sent to ${url}: Status ${response.status}`);
+    } catch (error) {
+      console.error(`Error self-pinging: ${error.message}`);
+    }
+  }, 1 * 60 * 1000); // 10 นาที (หน่วยเป็นมิลลิวินาที)
 });
